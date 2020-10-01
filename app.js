6923130d1bb3891c26cf64d9381a2c414fdf667c
yalitzab <yalitzabueno@gmail.com>
2020-09-30 20:39:59 -0700

    update user input js

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length)
-console.log(profileDataArgs);
-
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.lenght; i++){
+    // this
+    for (let i = 0; i < profileDataArr.lenght; i += 1){
         console.log(profileDataArr[i]);
     }
-}
 
-console.log('=====================');
+    console.log('=====================');
 
-// is the same as this...
-profileDataArr.forEach((profileItem) => console.log(profileItem));
-
+    // is the same as this...
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
 
 printProfileData(profileDataArgs);
