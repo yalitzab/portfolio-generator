b854cdd83a66581f507febed8771e826476a50c5
yalitzab <yalitzabueno@gmail.com>
2020-09-30 20:29:23 -0700

    learned about node.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length)
+console.log(profileDataArgs);
+
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.lenght; i++){
+        console.log(profileDataArr[i]);
+    }
+}
+
+console.log('=====================');
+
+// is the same as this...
+profileDataArr.forEach((profileItem) => console.log(profileItem));
+
+
+printProfileData(profileDataArgs);
