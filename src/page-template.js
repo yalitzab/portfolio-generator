514861054ee423e8ff543198915358f879ca3ea8
yalitzab <yalitzabueno@gmail.com>
2021-10-07 19:34:54 -0700

    create project section HTML

--- src/page-template.js
+++ src/page-template.js
@@ -12,6 +12,49 @@ const generateAbout = aboutText => {
     `;
   };
 
+  const generateProjects = projectsArr => {
+    return `
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+          .filter(({ feature }) => feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 mb-2 bg-dark text-light p-3">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
+          })
+          .join('')}
+  
+        ${projectsArr
+          .filter(({ feature }) => !feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
+          })
+          .join('')}
+        </div>
+      </section>
+    `;
+  };
+
 module.exports = templateData => {
     const { projects, about, ...header } = templateData;
     // console.log(templateData);
@@ -44,6 +87,7 @@ module.exports = templateData => {
 
     <main class="container my-5">
           ${generateAbout(about)}
+          ${generateProjects(projects)}
     </main>
 
     <footer class="container text-center py-3">
@@ -54,4 +98,4 @@ module.exports = templateData => {
     `;
 };
 
-module.exports = generatePage;
+// module.exports = generatePage;
