d201022d02153c908d28dabcac8354bedb4a45bb
yalitzab <yalitzabueno@gmail.com>
2021-10-07 18:59:27 -0700

    create the About section HTML conditionally

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,17 @@
+// create the about section
+const generateAbout = aboutText => {
+    if (!aboutText) {
+      return '';
+    }
+  
+    return `
+      <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+      </section>
+    `;
+  };
+
 module.exports = templateData => {
     const { projects, about, ...header } = templateData;
     // console.log(templateData);
@@ -27,9 +41,11 @@ module.exports = templateData => {
         </nav>
       </div>
     </header>
-    <main class="container my-5">
 
+    <main class="container my-5">
+          ${generateAbout(about)}
     </main>
+
     <footer class="container text-center py-3">
       <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
     </footer>
